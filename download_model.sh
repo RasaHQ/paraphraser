10feab4a65305d24d5d627def49db5c0988039aa
Daksh <d.varshneya@rasa.com>
2021-04-01 08:37:27 +0200

    add ssl skip for curl

--- download_model.sh
+++ download_model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 fileid="1x8eQttn7x2TtqMbKpAzuPc31-zY9sqZr"
 filename="m39v1.tar"
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+curl -k -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -k -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
 rm -r cookie
